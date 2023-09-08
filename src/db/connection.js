ed3cbd6b6774929eb91cbaf43674e5ec63988988
Zvi Schvarcz <zvischvarcz@gmail.com>
2023-09-08 13:43:00 +0100

    Readded max connections

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,6 +1,12 @@
 const { Sequelize } = require("sequelize")
 
-const connection = new Sequelize(process.env.DB_CONNECTION_URI)
+const connection = new Sequelize(process.env.DB_CONNECTION_URI, {
+
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+      }})
 
 connection.authenticate()
 
