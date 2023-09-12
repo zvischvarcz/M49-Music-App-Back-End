652b068d575debcd6b6111bc65eb32ad3fbdbf6a
Zvi Schvarcz <zvischvarcz@gmail.com>
2023-09-12 13:43:54 +0100

    Added token check for get all users

--- src/users/routes.js
+++ src/users/routes.js
@@ -10,7 +10,7 @@ userRouter.post("/users/register", hashPass, registerUser)
 userRouter.post("/users/login", comparePass, login)
 userRouter.put("/users/update", tokenCheck, emailCheck, updateUser)
 userRouter.delete("/users/delete", tokenCheck, deleteUser)
-userRouter.get("/users/all", getAllUsers)
+userRouter.get("/users/all", tokenCheck, getAllUsers)
 userRouter.get("/users/authCheck", tokenCheck, login)
 userRouter.get("/users/search", search)
 
